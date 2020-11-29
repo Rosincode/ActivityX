dfe3d5ffd921d2293ef64aa112c56d21edd1213a
Kevin Rosink <kevin@MacBook-Pro-van-Kevin.local>
2020-11-29 11:26:43 +0100

    Initial commit

---
+++ app/src/main/java/nl/thairosi/activityx/MainActivity.kt
@@ -0,0 +1,18 @@
+package nl.thairosi.activityx
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import nl.thairosi.activityx.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
