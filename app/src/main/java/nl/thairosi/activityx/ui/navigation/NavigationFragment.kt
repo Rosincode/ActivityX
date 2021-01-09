e4ec06e31fb09938ab7284839ef272284bf5f06d
kevin <kevinrosink@me.com>
2021-01-09 20:14:39 +0100

    Improved NavigationFragment.kt SOLID design patterns

--- app/src/main/java/nl/thairosi/activityx/ui/navigation/NavigationFragment.kt
+++ app/src/main/java/nl/thairosi/activityx/ui/navigation/NavigationFragment.kt
@@ -20,11 +20,28 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import nl.thairosi.activityx.R
 import nl.thairosi.activityx.databinding.FragmentNavigationBinding
+import nl.thairosi.activityx.models.Place
 import java.text.SimpleDateFormat
 import java.util.*
 import java.time.LocalDateTime as LocalDateTime
 
 class NavigationFragment : Fragment() {
+    //Properties
+    private lateinit var location: Location
+    private lateinit var place: Place
+    private var orientation: Float = 0F
+    private var distance: Float = 10000F
+    private var radius: String = "2000"
+    private var type: String = "bar"
+    private var maxPrice: String = "2"
+    private var initialDistance = 0.0F
+
+    //Flags
+    private var noPlace = true
+    private var noActivityFoundCount = 1
+    private var logAddress = true
+    private var setInitialDistance = true
+    private var placeAddedToDatabase = false
 
     private val viewModel: NavigationViewModel by lazy {
         ViewModelProvider(this).get(NavigationViewModel::class.java)
@@ -43,83 +60,25 @@ class NavigationFragment : Fragment() {
         // Giving the binding access to the ViewModel
         binding.navigationViewModel = viewModel
 
-//        binding.navigationRevealButton.setOnClickListener { v: View ->
-//            v.findNavController()
-//                .navigate(NavigationFragmentDirections.actionNavigationFragmentToPlaceFragment(
-//                    Place(
-//                        "ChIJW5MOkVpvxkcRDYqgo2pLGBY",
-//                        "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=ATtYBwK4_gAWP97d78u_JjiTe6TimhiICXvYF0Ww2NlM--JD3CagpFHv2s13vRAkjKSvXG4hg2Tum4ecFPUrB1BDaOQZJX8eT2fUP3inYE6kqKwItZM0qJ6Hgm9QfX2VSzPRbFozclkwvh1kFFPPU_FwEmbd-ibzotmAntN936IXa6DxCP_n&key=AIzaSyCofXZEndG5WokT6i6n5fdMabW3IWkGiRc",
-//                        "Kafé België",
-//                        "Oudegracht 196, 3511 NR Utrecht, Netherlands",
-//                        "cafe, bar, restaurant, food, point of interest, establishment",
-//                        "https://maps.google.com/?cid=1592105389659294221",
-//                        locationConverter(),
-//                        date = LocalDateTime.parse("2021-01-04T18:50:53"),
-//                        true)))
-//        }
-
-        //search criteria value getters
-        val criteria = PreferenceManager.getDefaultSharedPreferences(context)
-        val radius = criteria.getInt("criteriaDistanceSeekBar", 20).toString() + "000"
-        val type = criteria.getString("criteriaTypePreference", "bar").toString()
-        val maxPrice = criteria.getString("criteriaPricePreference", "4").toString()
-//
-//        Log.i("navigation", radius + " | " + type + " | " + maxPrice)
-
-        //Flags to determine what functionality is to be executed within the observers
-        var findRandomPlace = true
-        var noActivityFoundCount = 0
-        var logAddress = true
-        var setInitialDistance = true
-        var initialDistance = 0.0F
-        var addedToDatabase = false
+        //Get search criteria settings
+        setCriteria()
 
         //LOCATION OBSERVATION
         viewModel.location.observe(viewLifecycleOwner, { location ->
             if (location.hasAccuracy()) {
-                //Uses the phones location and the search criteria to get a random place or not
-                if (findRandomPlace) {
-                    searchingActivity()
-                    GlobalScope.launch {
-                        if (viewModel.notFinishedActivity() != null) {
-                            findRandomPlace = false
-                        } else {
-                            val latLng = location.latitude.toString() + "," + location.longitude.toString()
-                            viewModel.getRandomPlace(latLng, radius, type, maxPrice)
-                            findRandomPlace = false
-                        }
-                    }
-
+                this.location = location
+                if (noPlace) {
+                    findPlace()
                 } else {
-                    //PLACE OBSERVATION
                     viewModel.place.observe(viewLifecycleOwner, { place ->
+                        this.place = place
                         if (place.location != null) {
-                            if (!addedToDatabase) {
-                                viewModel.addToDatabase(place)
-                                addedToDatabase = true
-                            }
-
-                            val distance = location.distanceTo(place.location)
-
-                            //Set the initial distance for percentage calculations
-                            if (setInitialDistance) {
-                                initialDistance = distance
-                                setInitialDistance = false
-                            }
-
-                            //Logs the randomly found address for demo purposes
-                            if (logAddress) {
-                                Log.i("navigation", place.name)
-                                logAddress = false
-                            }
-
-                            //Updates the bottom padding of the distance image
+                            logAddress()
+                            insertPlaceIntoDatabase()
+                            setInitialDistance()
+                            setDistance()
                             binding.navigationDestinationImage.updatePaddingRelative(
-                                bottom = calculateDistanceImagePadding(
-                                    initialDistance,
-                                    distance
-                                )
-                            )
+                                bottom = calculateDistanceImagePadding())
 
                             //Informs the user he is nearby the activity shows the reveal button
                             if (distance < 50) {
@@ -128,7 +87,7 @@ class NavigationFragment : Fragment() {
                                     place.date = getDateTime()
                                     place.revealed = true
                                     viewModel.addToDatabase(place)
-                                    findRandomPlace = true
+                                    noPlace = true
                                     v.findNavController().navigate(NavigationFragmentDirections
                                         .actionNavigationFragmentToPlaceFragment(place))
                                 }
@@ -145,7 +104,6 @@ class NavigationFragment : Fragment() {
                         } else {
                             noActivityFound(noActivityFoundCount)
                             noActivityFoundCount++
-                            Log.i("navigation", noActivityFoundCount.toString())
                         }
                     })
                 }
@@ -156,17 +114,72 @@ class NavigationFragment : Fragment() {
         return binding.root
     }
 
-    fun getDateTime() : LocalDateTime {
+    private fun setCriteria() {
+        val criteria = PreferenceManager.getDefaultSharedPreferences(context)
+        radius = criteria.getInt("criteriaDistanceSeekBar", 20).toString() + "000"
+        type = criteria.getString("criteriaTypePreference", "bar").toString()
+        maxPrice = criteria.getString("criteriaPricePreference", "2").toString()
+        Log.i("navigation", "Search criteria: " +
+                "Radius = $radius, Type = $type, Maximum price = $maxPrice")
+    }
+
+    //Tries to find the correct place to be navigated to
+    private fun findPlace() {
+        Log.i("navigation", "Searching activity: Show toast & get place")
+        val text = getString(R.string.navigation_searching_activity_toast)
+        Toast.makeText(context, text, Toast.LENGTH_LONG).show()
+        GlobalScope.launch {
+            noPlace = if (viewModel.notFinishedActivity() != null) {
+                false
+            } else {
+                val latLng = location.latitude.toString() + "," + location.longitude.toString()
+                viewModel.getRandomPlace(latLng, radius, type, maxPrice)
+                false
+            }
+        }
+    }
+
+    //Logs the randomly found address for demo purposes
+    private fun logAddress() {
+        if (logAddress) {
+            Log.i("navigation", place.name)
+            logAddress = false
+        }
+    }
+
+    private fun insertPlaceIntoDatabase() {
+        if (!placeAddedToDatabase) {
+            viewModel.addToDatabase(place)
+            Log.i("navigation", "Place added to database: ${place.name}")
+            placeAddedToDatabase = true
+        }
+    }
+
+    //Set the initial distance for percentage calculations
+    private fun setInitialDistance() {
+        if (setInitialDistance) {
+            initialDistance = distance
+            setInitialDistance = false
+        }
+    }
+
+    private fun setDistance() {
+        distance = location.distanceTo(place.location)
+    }
+
+    //Returns the current local date and time
+    private fun getDateTime() : LocalDateTime {
         val dateFormat = SimpleDateFormat(
             "yyyy-MM-dd", Locale.getDefault());
         val dateFormat2 = SimpleDateFormat(
             "HH:mm:ss", Locale.getDefault());
         val date = Date();
-        return LocalDateTime.parse(dateFormat.format(date) + "T" + dateFormat2.format(date));
+        Log.i("navigation", "LocalDateTime added to this place in the database: $date")
+        return LocalDateTime.parse(dateFormat.format(date) + "T" + dateFormat2.format(date))
     }
 
     //Calculates the necessary padding for the distance image in the fragment ui
-    private fun calculateDistanceImagePadding(initialDistance: Float, distance: Float): Int {
+    private fun calculateDistanceImagePadding(): Int {
         //Calculates the percentage of the distance over the initialDistance less than 100%
         return if (distance < initialDistance) {
             val percentage = 100 * (distance / initialDistance)
@@ -176,28 +189,14 @@ class NavigationFragment : Fragment() {
             if (distance > (initialDistance + 100)) {
                 wrongDirection()
             }
-            250 //returns 250 when over 100% as the max padding
+            250 //returns 250 as max padding when the distance is over the initial distance
         }
-    }
-
-    //To test
-    private fun locationConverter(): Location {
-        val androidLocation = Location(LocationManager.GPS_PROVIDER)
-        androidLocation.latitude = 52.08915712791165
-        androidLocation.longitude = 5.12157871534323
-        //
-        return androidLocation
-    }
-
-    //Actions on started activity search (Toast)
-    private fun searchingActivity() {
-        val text = getString(R.string.navigation_searching_activity_toast)
-        Toast.makeText(context, text, Toast.LENGTH_LONG).show()
     }
 
     //Toast & navigateUp: Actions on no activity found
     private fun noActivityFound(noActivityFoundCount: Int) {
-        if (noActivityFoundCount > 1) {
+        Log.i("navigation", "Searching activity attempt: $noActivityFoundCount")
+        if (noActivityFoundCount > 2) {
             val text = getString(R.string.navigation_no_activity_found_toast)
             Toast.makeText(context, text, Toast.LENGTH_LONG).show()
             findNavController().navigateUp()
@@ -206,18 +205,21 @@ class NavigationFragment : Fragment() {
 
     //Actions on no accuracy found
     private fun noAccuracy() {
+        Log.i("navigation", "No Accuracy: Show toast")
         val text = getString(R.string.navigation_no_accuracy_toast)
         Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
     }
 
     //Actions on wrong direction
     private fun wrongDirection() {
+        Log.i("navigation", "Wrong direction: Show toast")
         val text = getString(R.string.navigation_wrong_direction_toast)
         Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
     }
 
     //Actions on within 50 meters of the activity
     private fun nearbyActivity() {
+        Log.i("navigation", "Nearby activity: Show toast & reveal button")
         val text = getString(R.string.navigation_nearby_activity_toast)
         Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
         navigationRevealButton.visibility = View.VISIBLE
